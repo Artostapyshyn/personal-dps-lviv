61535dd78f156f1537640d86cb7d2314e670f16d
Artostapyshyn <artostapyshyn@gmail.com>
2022-12-26 09:23:47 +0200

    Updates in authorization controller

--- src/main/java/com/artostapyshyn/personaldpslviv/controller/AuthController.java
+++ src/main/java/com/artostapyshyn/personaldpslviv/controller/AuthController.java
@@ -1,36 +1,55 @@
 package com.artostapyshyn.personaldpslviv.controller;
 
+import javax.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
 import com.artostapyshyn.personaldpslviv.model.entity.Employee;
+import com.artostapyshyn.personaldpslviv.model.entity.Role;
+import com.artostapyshyn.personaldpslviv.model.service.EmployeeService;
 
-
+@AllArgsConstructor
 @Controller
 public class AuthController {
 
-	@GetMapping("/")
+	private final EmployeeService employeeService;
+	private final PasswordEncoder passwordEncoder;
+
+	@GetMapping("/") 
 	public String getHomePage() {
 		return "home";
 	}
-	
-    @GetMapping("/login")
-    public String getLoginPage() {
-        return "login";
-    }
+ 
+	@GetMapping("login")
+	public String getLoginPage() {
+		return "login";
+	}
 
-    @GetMapping("/profile")
-    public String getSuccessPage() {
-        return "redirect:/";
-    }
+ 	@GetMapping("profile")
+	public String getSuccessPage() {
+		return "redirect:/";
+	}
 
-    @GetMapping("/registration")
-    public String getRegisterPage(Model model) {
-        model.addAttribute("user", new Employee());
-        return "registration";
-    }
+	@GetMapping("registration")
+	public String getRegisterPage(Model model) {
+		model.addAttribute("user", new Employee());
+		model.addAttribute("roles", Role.values());
+		return "registration";
+	}
 
+	@PostMapping("registration")
+	public String postRegisterPage(@Valid Employee employee, BindingResult result, Model model) {
+		if (HelperController.hasErrors(result, model)) {
+			model.addAttribute("roles", Role.values());
+			return "register";
+		}
 
-
+		employee.setPassword(passwordEncoder.encode(employee.getPassword()));
+		employeeService.saveAndFlush(employee);
+		return "redirect:/";
+	}
 }
